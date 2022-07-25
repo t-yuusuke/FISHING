4f1b55c5db797fc2698e547b893f8fdc058db431
t-yuusuke <1127yuusuke56@gmail.com>
2022-07-25 13:33:59 +0000

    もう少し

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 }
 
 scope module: :public do
-  get "top" => "homes#top", as: 'top'
+  root to: "homes#top"
   get "about" => "homes#about", as: 'about'
   get "search" => "searches#search"
   resources :notifications, only: [:index]
